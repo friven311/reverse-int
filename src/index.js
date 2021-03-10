eea26d84a56ad9cb22c982402736db277a0f2468
friven311 <77752535+friven311@users.noreply.github.com>
2021-03-10 18:45:54 +0300

    init start reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+  if (n < 0){
+     n = -n;
+    var str = String(n);
+    var z = str.split("").reverse().join("");
+    return z;
+  }
+  var str = String(n);
+  var z = str.split("").reverse().join("");
+  return z;
 }
